08b3f2a99b08fc839e1f0c6840e7c1adf766242f
facundogarciaengel <fgarciaengel@gmail.com>
2023-07-27 19:56:02 -0300

    creacion de rutas

--- app..js
+++ app..js
@@ -2,3 +2,33 @@ const express = require("express");
 const app = express(); 
 const path = require("path"); 
 app.use(express.static(path.join(__dirname, "/public")))
+app.listen(3000, ()=>{
+    console.log("Servidor corriendo en el puerto 3000")
+})
+
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.get("/registro", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/registro.html"))
+})
+
+app.get("/login", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+
+app.get("/carrito", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/carrito.html"))
+})
+
+app.get("/producto", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/producto.html"))
+})
+
+
+
+
+
+
+
