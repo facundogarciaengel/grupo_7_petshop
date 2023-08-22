5d59aca5bfd94cf5215322b59e8f052d3770db19
FelipeCardozo96 <felipemcr996@gmail.com>
2023-08-21 22:14:31 -0300

    Se agregó EJS al proyecto, se modificaron las extensiones de los archivos html. Se creó carpeta PARTIALS que contiene elementos que se repiten en otras vistas (head, header, footer).

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 const express = require("express");
 const app = express(); 
+
+app.set("view engine", "ejs")
+
 const path = require("path"); 
+
 app.use(express.static(path.join(__dirname, "/public")))
+
 app.listen(3000, ()=>{
     console.log("Servidor corriendo en el puerto 3000")
 })
 
 app.get("/", (req,res)=>{
-    res.sendFile(path.join(__dirname, "/views/home.html"))
+    res.sendFile(path.join(__dirname, "/views/home.ejs"))
 })
 
 app.get("/registro", (req,res)=>{
-    res.sendFile(path.join(__dirname, "/views/registro.html"))
+    res.sendFile(path.join(__dirname, "/views/registro.ejs"))
 })
 
 app.get("/login", (req,res)=>{
-    res.sendFile(path.join(__dirname, "/views/login.html"))
+    res.sendFile(path.join(__dirname, "/views/login.ejs"))
 })
 
 app.get("/carrito", (req,res)=>{
-    res.sendFile(path.join(__dirname, "/views/carrito.html"))
+    res.sendFile(path.join(__dirname, "/views/carrito.ejs"))
 })
 
 app.get("/producto", (req,res)=>{
-    res.sendFile(path.join(__dirname, "/views/producto.html"))
+    res.sendFile(path.join(__dirname, "/views/producto.ejs"))
 })
 
 
